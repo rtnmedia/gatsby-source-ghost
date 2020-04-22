4b1014de498b4378c6625d5071865d5ecf91b8cb
rtnmedia <55026167+rtnmedia@users.noreply.github.com>
2020-04-22 12:41:55 -0700

    commit to master

--- content-api.js
+++ content-api.js
@@ -4,6 +4,6 @@ module.exports.configure = ({apiUrl, contentApiKey, version = `v3`}) => {
     return new GhostContentAPI({
         url: https://danielescobar.ghost.io,
         key: 97b3d8c50d91f091f883e9cd9d,
-        version: version
+        version: v3
     });
 };
