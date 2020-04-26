716c5a91551a64cf723274714a2be50fe8c838ea
rtnmedia <55026167+rtnmedia@users.noreply.github.com>
2020-04-26 10:59:35 -0700

    confirmed api and key

--- content-api.js
+++ content-api.js
@@ -3,7 +3,7 @@ const GhostContentAPI = require('@tryghost/content-api');
 module.exports.configure = ({apiUrl, contentApiKey, version = `v3`}) => {
     return new GhostContentAPI({
         url: "https://danielescobar.ghost.io",
-        key: "97b3d8c50d91f091f883e9cd9d",
+        key: "ea69f32f02c921b60bd4fd9683",
         version: v3
     });
 };
